01fa0e44618ed3876e3cf0ceec19086232b1adb3
Jeff Hollan <jehollan@microsoft.com>
2018-07-29 20:48:37 -0700

    working with integration tests

---
+++ FunctionApp.Tests.Integration/IoTEndToEndTests.cs
@@ -0,0 +1,46 @@
+﻿using FunctionTestHelper;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FunctionApp.Tests.Integration
+{
+    [Collection("Function collection")]
+    public class IoTEndToEndTests : EndToEndTestsBase<TestFixture>
+    {
+        private readonly ITestOutputHelper output;
+        public IoTEndToEndTests(TestFixture fixture, ITestOutputHelper output) : base(fixture)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public async Task IoTHub_TriggerFires()
+        {
+
+            string connectionString = Environment.GetEnvironmentVariable("DeviceConnectionString");
+
+            var iotClient = DeviceClient.CreateFromConnectionString(connectionString);
+            string guid = Guid.NewGuid().ToString();
+            try
+            {
+                await iotClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes("Test" + guid)));
+
+                await WaitForTraceAsync("ButtonPressAsync", log => log.FormattedMessage.Contains(guid));
+                output.WriteLine(Fixture.Host.GetLog());
+            }
+            catch(Exception ex)
+            {
+                output.WriteLine(Fixture.Host.GetLog());
+                throw ex;
+            }
+        }
+    }
+}
