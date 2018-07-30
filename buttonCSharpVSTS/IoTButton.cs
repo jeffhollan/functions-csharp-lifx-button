378a5b70a5caf3e7fccc1cb8638537b27bedac3b
Jeff Hollan <jehollan@microsoft.com>
2018-07-29 22:12:15 -0700

    Breaking feature to test PR

--- buttonCSharpVSTS/IoTButton.cs
+++ buttonCSharpVSTS/IoTButton.cs
@@ -20,8 +20,8 @@ namespace buttonCSharpVSTS
             [IoTHubTrigger("messages/events", Connection = "EventHubConnectionString")]EventData message, 
             ILogger log)
         {
-            log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
-
+            // log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+            log.LogInformation("A change that should break the build");
             HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Post, "https://api.lifx.com/v1/lights/all/toggle");
             req.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", Environment.GetEnvironmentVariable("LIFX_TOKEN"));
 
